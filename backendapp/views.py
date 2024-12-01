964f9a03202d8a7dd2b64c3fb740007eaef36f11
sriram <sriramachandran28@gmail.com>
2024-12-01 11:23:28 -0400

    Initialize django app

---
+++ backendapp/views.py
@@ -0,0 +1,7 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from backendApp.models import *
+
+
+#def index(request):
+#    return render(request,"backendApp/index.html",context)
